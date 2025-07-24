854c506213622fe95285970beaf564f379a3b270
김선경 <mimcom43@gmail.com>
2025-07-24 23:57:19 +0900

    Refactor : js 및 흐름 개선
    - BookLayout 변수/함수명/주석명 등 직관적 개선
    - 도서 목록 노출 제어
    - 도서 선택 비활성 css 수정

--- src/pages/BookLayout.jsx
+++ src/pages/BookLayout.jsx
@@ -6,17 +6,17 @@ import CurrentBooksList from "../components/CurrentBooksList";
 
 function BookLayout(props) {
   const [users] = useState(["김선경", "홍길동"]);
-  const [currentUser, setCurrentUser] = useState("");
-  const [myName, setMyName] = useState("");
-  const [inputValue, setInputValue] = useState("");
+  const [currentUser, setCurrentUser] = useState(""); // 유저 선택
+  const [myName, setMyName] = useState(""); // 로그인 유저
+  const [inputValue, setInputValue] = useState(""); // 책 이릅력값
 
-  // 도서 목록
+  // 전체 목록
   const [books, setBooks] = useState([
     { book: "책제목1", name: "", chk: false },
     { book: "책제목2", name: "", chk: false },
   ]);
 
-  // 대여한 도서 목록
+  // 대여 목록
   const [myBooks, setMyBooks] = useState([
     { book: "책제목1_선경", name: "김선경", chk: false },
     { book: "책제목2_선경", name: "김선경", chk: false },
@@ -33,7 +33,7 @@ function BookLayout(props) {
     setMyName(currentUser);
   };
 
-  // checkbox chk value 제어
+  // 도서 선택 toggle
   const toggleBookCheck = (index) => {
     setBooks((prevBooks) =>
       prevBooks.map((book, i) =>
@@ -48,7 +48,8 @@ function BookLayout(props) {
       alert("값을 입력해주세요.");
       return;
     }
-    setBooks((prev) => [...prev, { book: inputValue, name: "", chk: false }]);
+    const newBook = { book: inputValue, name: "", chk: false };
+    setBooks((prev) => [...prev, newBook]);
     setInputValue("");
   };
 
@@ -60,31 +61,43 @@ function BookLayout(props) {
       return;
     }
 
-    // 추가
-    setMyBooks([
-      ...myBooks,
-      ...checkedBooks.map((book) => ({ ...book, name: myName, chk: false })),
-    ]);
+    // 선택된 책들 myBooks에 대여
+    const rentedList = checkedBooks.map((book) => ({
+      ...book,
+      name: myName,
+      chk: false, // 대여 후엔 체크 해제
+    }));
+    setMyBooks([...myBooks, ...rentedList]);
 
     // name만 바꾸고 chk 초기화
-    const updatedBooks = books.map((book) =>
+    const updatedBooksList = books.map((book) =>
       book.chk ? { ...book, name: myName, chk: false } : book
     );
-    setBooks(updatedBooks);
+    setBooks(updatedBooksList);
   };
 
   // 반납
   const evtReturn = () => {
-    const checked = myBooks.filter((book) => book.chk && book.name === myName);
-    if (checked.length === 0) {
+    const returnList = myBooks.filter(
+      (book) => book.chk && book.name === myName
+    );
+
+    if (returnList.length === 0) {
       alert("반납할 책이 없습니다");
       return;
     }
-    setMyBooks(myBooks.filter((prev) => !(prev.chk && prev.name === myName)));
-    setBooks((prev) => [
-      ...prev.filter((prev) => !checked.some((c) => c.book === prev.book)),
-      ...checked.map((prev) => ({ ...prev, chk: false, name: "" })),
-    ]);
+
+    // myBooks에서 반납한 책 삭제
+    const updatedMyBooksList = myBooks.filter(
+      (book) => !(book.chk && book.name === myName)
+    );
+    setMyBooks(updatedMyBooksList);
+
+    // books에서 반납했던 책 복구
+    const updatedBooksList = books
+      .filter((book) => !returnList.some((item) => item.book === book.book))
+      .concat(returnList.map((book) => ({ ...book, name: "", chk: false })));
+    setBooks(updatedBooksList);
   };
 
   // 삭제
@@ -112,23 +125,25 @@ function BookLayout(props) {
         />
 
         {myName !== "" && (
-          <CurrentBooksList
-            myBooks={myBooks.filter((item) => item.name === myName)} // 내가 선택한 users에 맞게 myBook 뿌리기
-            evtReturn={evtReturn}
-            setMyBooks={setMyBooks}
-          />
-        )}
+          <div>
+            <CurrentBooksList
+              myBooks={myBooks.filter((item) => item.name === myName)} // 내가 선택한 users에 맞게 myBook 뿌리기
+              evtReturn={evtReturn}
+              setMyBooks={setMyBooks}
+            />
 
-        <BooksList
-          books={books}
-          evtAdd={evtAdd}
-          evtRental={evtRental}
-          evtDelete={evtDelete}
-          inputValue={inputValue}
-          setInputValue={setInputValue}
-          setBooks={setBooks}
-          toggleBookCheck={toggleBookCheck}
-        />
+            <BooksList
+              books={books}
+              evtAdd={evtAdd}
+              evtRental={evtRental}
+              evtDelete={evtDelete}
+              inputValue={inputValue}
+              setInputValue={setInputValue}
+              setBooks={setBooks}
+              toggleBookCheck={toggleBookCheck}
+            />
+          </div>
+        )}
       </main>
     </>
   );
